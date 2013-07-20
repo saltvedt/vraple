f0a4c961ef8651c13767025901e5846f59c2ad1c
Torstein Saltvedt <torstein.saltvedt@gmail.com>
2013-07-20 10:59:48 +0200

    Added basic backbone structure for project.

--- js/vraple.js
+++ js/vraple.js
@@ -1,4 +1,24 @@
 
 $(function(){
-	//nothing here yet
+	window.App = {
+		Router: {}
+	};
+
+	App.Router = Backbone.Router.extend({
+		routes: {
+			'': 'index',
+			'q=:query': 'search'
+		},
+
+		index: function() {
+			//this is were we display the info on the main page
+		},
+
+		search: function(query) {
+			//parse the query and determine what search engine to send the user to
+		}
+	});
+
+	new App.Router();
+	Backbone.history.start();
 });
