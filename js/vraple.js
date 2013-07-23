040f0e497eb3afac57e65e307b06281bfeed639b
Torstein Saltvedt <torstein.saltvedt@gmail.com>
2013-07-23 21:16:58 +0200

    Minimum working vraple. Support for Duck Duck Go, fall back to Google. Only tested in Opera.

--- js/vraple.js
+++ js/vraple.js
@@ -1,3 +1,4 @@
+window.onunload = function(){};
 
 $(function(){
 	window.App = {
@@ -7,15 +8,44 @@ $(function(){
 	App.Router = Backbone.Router.extend({
 		routes: {
 			'': 'index',
-			'q=:query': 'search'
+			'q=:query': 'vraple',
+			'went_to=:search_engine&q=:query' : 'default'
 		},
 
 		index: function() {
 			//this is were we display the info on the main page
 		},
 
-		search: function(query) {
-			//parse the query and determine what search engine to send the user to
+		vraple: function(query) {
+			//parse the query and determine what search engine to send the user
+
+			//anything with an ! in it, obviously not ideal.
+			var duckduckgo = new RegExp(/!/);
+
+			if (duckduckgo.test(query)) {
+				goto_url = "http://duckduckgo.com/?q=" + query;
+				url_data = "went_to=duckduckgo&q=" + query;
+			} else {
+				goto_url = "https://www.google.com/search?q=" + query;
+				url_data = "went_to=google&q=" + query;
+			}
+
+			//add the params of which search engine we're going to send the user to the history of the browser,
+			//so that when the user hits the back button, we can redirect them to google.
+			Backbone.history.navigate(url_data);
+			window.location = goto_url;
+		},
+
+		default: function(search_engine, query) {
+			//for now, every "failed" vraple goes to google
+			//note that this feature will probably never work for chrome (at least not in this way) because chrome refuses to redirect
+			//dont redirect to google more than once
+			if (search_engine !== "google") {
+				goto_url = "https://www.google.com/search?q=" + query;
+				url_data = "went_to=google&q=" + query;
+				Backbone.history.navigate(url_data);
+				window.location = goto_url;
+			}
 		}
 	});
 
